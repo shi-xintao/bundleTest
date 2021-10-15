710bd7684c277f6c9eb514f7a308b2c7422274f3
Ziliang.guo <Ziliang.guo@pplingo.com>
2021-10-15 16:59:11 +0800

    fix

---
+++ web-mobile/assets/main/index.c2dc2.js
@@ -0,0 +1,87 @@
+window.__require = function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var b = o.split("/");
+        b = b[b.length - 1];
+        if (!t[b]) {
+          var a = "function" == typeof __require && __require;
+          if (!u && a) return a(b, !0);
+          if (i) return i(b, !0);
+          throw new Error("Cannot find module '" + o + "'");
+        }
+        o = b;
+      }
+      var f = n[o] = {
+        exports: {}
+      };
+      t[o][0].call(f.exports, function(e) {
+        var n = t[o][1][e];
+        return s(n || e);
+      }, f, f.exports, e, t, n, r);
+    }
+    return n[o].exports;
+  }
+  var i = "function" == typeof __require && __require;
+  for (var o = 0; o < r.length; o++) s(r[o]);
+  return s;
+}({
+  Helloworld: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "e1b90/rohdEk4SdmmEZANaD", "Helloworld");
+    "use strict";
+    var __extends = this && this.__extends || function() {
+      var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf || {
+          __proto__: []
+        } instanceof Array && function(d, b) {
+          d.__proto__ = b;
+        } || function(d, b) {
+          for (var p in b) Object.prototype.hasOwnProperty.call(b, p) && (d[p] = b[p]);
+        };
+        return extendStatics(d, b);
+      };
+      return function(d, b) {
+        extendStatics(d, b);
+        function __() {
+          this.constructor = d;
+        }
+        d.prototype = null === b ? Object.create(b) : (__.prototype = b.prototype, new __());
+      };
+    }();
+    var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
+      var c = arguments.length, r = c < 3 ? target : null === desc ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+      if ("object" === typeof Reflect && "function" === typeof Reflect.decorate) r = Reflect.decorate(decorators, target, key, desc); else for (var i = decorators.length - 1; i >= 0; i--) (d = decorators[i]) && (r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r);
+      return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+    var Helloworld = function(_super) {
+      __extends(Helloworld, _super);
+      function Helloworld() {
+        var _this = null !== _super && _super.apply(this, arguments) || this;
+        _this.label = null;
+        _this.text = "hello";
+        return _this;
+      }
+      Helloworld.prototype.start = function() {
+        this.label.string = this.text;
+      };
+      __decorate([ property(cc.Label) ], Helloworld.prototype, "label", void 0);
+      __decorate([ property ], Helloworld.prototype, "text", void 0);
+      Helloworld = __decorate([ ccclass ], Helloworld);
+      return Helloworld;
+    }(cc.Component);
+    exports.default = Helloworld;
+    cc._RF.pop();
+  }, {} ],
+  "use_v2.1-2.2.1_cc.Toggle_event": [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "a42e0TUJDFKH4PGEpRIhfua", "use_v2.1-2.2.1_cc.Toggle_event");
+    "use strict";
+    cc.Toggle && (cc.Toggle._triggerEventInScript_isChecked = true);
+    cc._RF.pop();
+  }, {} ]
+}, {}, [ "Helloworld", "use_v2.1-2.2.1_cc.Toggle_event" ]);
