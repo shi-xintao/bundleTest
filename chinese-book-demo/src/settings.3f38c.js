cda1f1973851a4b591d0fefeaaef4c6d09f14554
Ziliang.guo <Ziliang.guo@pplingo.com>
2021-10-21 16:07:52 +0800

    绘本demo

---
+++ chinese-book-demo/src/settings.3f38c.js
@@ -0,0 +1,54 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "exploreGame",
+        "bookplayer"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/main.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "c363d",
+        common: "38aba",
+        Bubble: "509f1",
+        SweepLeaves: "0c792",
+        book_player: "498af",
+        exploreGame: "d7ee4",
+        game_lesson_5: "9ce31",
+        paint: "a7c08",
+        paintingColor: "03457",
+        picture_balloon: "091f7",
+        quizGameBlue: "816f3",
+        quizGameRed: "c78f3",
+        quziGamePanda: "3bce7",
+        redSantaClaus: "8b6f7",
+        rocketProject: "10253",
+        templateGame: "0eb93",
+        video: "e09e7",
+        Float: "f5fd7",
+        common_en: "8d784",
+        eatBeans: "c8767",
+        hippo: "c9a24",
+        main: "d9407"
+    }
+};
