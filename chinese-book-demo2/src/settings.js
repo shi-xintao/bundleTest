db949ff40d5ce846b1e0afabfb400a7230d5d60c
Ziliang.guo <Ziliang.guo@pplingo.com>
2021-10-22 16:52:58 +0800

    fix

---
+++ chinese-book-demo2/src/settings.js
@@ -0,0 +1,30 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "exploreGame",
+        "bookplayer"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/main.fire",
+    orientation: "",
+    debug: true,
+    jsList: []
+};
